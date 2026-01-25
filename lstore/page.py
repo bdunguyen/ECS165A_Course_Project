6ec351daa412a1495d36a6cadad0f8b7f85b0fd3
aw1192 <256239685+aw1192@users.noreply.github.com>
2026-01-24 17:13:59 -0800

    Update page.py

--- lstore/page.py
+++ lstore/page.py
@@ -4,11 +4,23 @@ class Page:
     def __init__(self):
         self.num_records = 0
         self.data = bytearray(4096)
+        self.curr = 0
 
     def has_capacity(self):
-        pass
+        if self.num_records < len(self.data):
+            return True
+        else:
+            return False
 
     def write(self, value):
-        self.num_records += 1
-        pass
+        if self.has_capacity():
+            for i in range(len(value)):
+                self.data[self.num_records] = ord(i)
+                self.curr += 1
+            self.num_records += 1
+
+            return "Write successful!"
+        else:
+            return "Failed to write."
+
 
