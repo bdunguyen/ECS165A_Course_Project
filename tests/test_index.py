240b9b5776b36f2766497499652cee5a8aae907f
bri <73381026+bdunguyen@users.noreply.github.com>
2026-01-26 12:22:55 -0800

    Create test_index.py

---
+++ tests/test_index.py
@@ -0,0 +1,27 @@
+"""
+# returns the location of all records with the given value on column "column"
+"""
+def test_locate():
+    # locate(column, value)
+    pass
+
+"""
+# Returns the RIDs of all records with values in column "column" between "begin" and "end"
+"""
+def test_locate_range():
+    # locate_range(self, begin, end, column)
+    pass
+
+"""
+# optional: Create index on specific column
+"""
+def test_create_index():
+    # create_index(self, column_number)
+    pass
+
+"""
+# optional: Drop index of specific column
+"""
+def test_drop_index():
+    # drop_index(self, column_number)
+    pass
