8f39a58cbcf120c74bb35bf1e8b72857750aba66
Jodi Liu <jodiliu2@gmail.com>
2026-01-27 19:14:09 -0800

    Update test_page.py

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,5 +1,18 @@
+from lstore.db import Page
 def test_has_capacity(self, value):
-    pass
+    page = Page()
+    value = "hello"
+
+    assert page.has_capacity(value) is True
 
 def test_write(self, value):
-    pass
+    page = Page()
+    value = "abc"
+
+    result = page.write(value)
+    assert result == "Write successful!"
+    assert page.num_records == 1
+    assert page.curr == len(value)
+
+    # check data was written correctly
+    assert page.data[0:3] == bytearray(b"abc")
