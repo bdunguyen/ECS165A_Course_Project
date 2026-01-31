051e17d74b0f714613b8293faa6b5571c2a9300a
bri <73381026+bdunguyen@users.noreply.github.com>
2026-01-31 14:48:23 -0800

    Update test_page.py

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,16 +1,17 @@
 from lstore.page import Page
-def test_has_capacity(self, value):
+
+def test_has_capacity():
     page = Page()
     value = "hello"
 
     assert page.has_capacity(value) is True
 
-def test_write(self, value):
+def test_write():
     page = Page()
     value = "abc"
 
     result = page.write(value)
-    assert result == "Write successful!"
+    assert result == True
     assert page.num_records == 1
     assert page.curr == len(value)
 
