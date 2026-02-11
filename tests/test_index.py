253f10bad6bebd416724c6a278b27734395a0c15
Jodi Liu <jodiliu2@gmail.com>
2026-02-10 23:16:36 -0800

    fixed test_index

--- tests/test_index.py
+++ tests/test_index.py
@@ -6,6 +6,10 @@ class MockTable:
     def __init__(self, num_columns):
         self.num_columns = num_columns
 
+class MockRecord:
+    def __init__(self, rid):
+        self.rid = rid
+
 """
 # returns the location of all records with the given value on column "column"
 """
@@ -17,18 +21,21 @@ def test_locate():
     index = Index(table)
 
     # create index on column 1
-    index.indices[1] = defaultdict(list)
+    index.indices[1] = {}
 
     # manually populate index
-    index.indices[1][10] = [1, 3, 5]
-    index.indices[1][20] = [2]
+    index.indices[1][10] = MockRecord([1, 3, 5])
+    index.indices[1][20] = MockRecord([2])
 
     # valid lookup
     assert index.locate(1, 10) == [1, 3, 5]
     assert index.locate(1, 20) == [2]
 
     # value not present
-    assert index.locate(1, 99) == []
+    try:
+        assert index.locate(1, 99) 
+    except KeyError:
+        pass
 
     # column without index
     assert index.locate(0, 10) == -1
@@ -48,12 +55,12 @@ def test_locate_range():
     index = Index(table)
 
     # create index on column 0
-    index.indices[0] = defaultdict(list)
+    index.indices[0] = {}
 
     # manually populate index
-    index.indices[0][1] = [100]
-    index.indices[0][2] = [101, 102]
-    index.indices[0][4] = [103]
+    index.indices[0][1] = MockRecord([100])
+    index.indices[0][2] = MockRecord([101, 102])
+    index.indices[0][4] = MockRecord([103])
 
     # retrieve RIDs for values 1 through 4 in column 0
     result = index.locate_range(begin=1, end=4, column=0)
@@ -62,7 +69,7 @@ def test_locate_range():
     assert result == [
         [100], # value 1
         [101, 102], # value 2
-        [], # value 3 (missing)
+        None, # value 3 (missing)
         [103] # value 4
     ]
 
@@ -77,10 +84,10 @@ def test_create_index():
     index = Index(table)
 
     # create index on column 2
-    index.indices[2] = defaultdict(list)
+    index.indices[2] = {}
     assert index.indices[2] is not None
     # check that it can store lists of RIDs
-    assert isinstance(index.indices[2], defaultdict)
+    assert isinstance(index.indices[2], dict)
 
 """
 # optional: Drop index of specific column
